c2790115bed5d5784282857a18ac202338ffddd6
ManalBen1 <manatini@gmail.com>
2023-11-14 08:02:00 -0800

    printf

---
+++ README.md
@@ -0,0 +1,2 @@
+C - printf
+Project to be done in teams of 2 people (your team: Tyronne Vanderpuye-Orgle, Manal Benhmida)
---
+++ main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+int _printf(const char *format, ...);
+
+int _putchar(char c);
+
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+_printf("%b\n", 98);
+return (0);
+
+_printf("%S\n", "Best\nSchool");
+return (0);
+}
