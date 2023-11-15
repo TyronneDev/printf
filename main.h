69b9d305d71e860f575c3c600f18ad35ace11212
ManalBen1 <manatini@gmail.com>
2023-11-14 17:19:18 -0800

    main.h

--- main.h
+++ main.h
@@ -1,41 +1,17 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h> 
-
-int _printf(const char *format, ...)
-{
-int i = 0;
-for(i = 0; i= i + 1=)
-{
-if (*format == %)
-{
-format = format + 1;
-
-if (*format == 'd'|| *format == 'i')
-{
-char buffer[20];
-int num = 42
-int len = sprintf(buffer, "%d", num);
-
-write(1, buffer, num);
-i += len;
-i = i + 1;
-}
-
-}
-}
-}
+#include <unistd.h>
 
 
-
-
-
+int _printf(const char *format, ...);
+int print_char (va_list character);
+int format_handler (char type, va_list list, int *position);
 int _putchar(char c);
-
-
-
+int print_str(va_list list);
+int print_percent(va_list list __attribute__((unused)));
+int print_int(va_list list);
 
 #endif /* MAIN_H */
